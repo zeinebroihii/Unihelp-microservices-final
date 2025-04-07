f60b9623dc99975186c9292a10ba579e057c7050
Raouf2022 <abderraouf.mahmoudi@esprit.tn>
2025-04-07 21:06:23 +0100

    last fixes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import {NotFoundComponent} from "./components/not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'login', component: LoginComponent},
+  {path:'home', component: HomeComponent},
+  { path: '**', component: NotFoundComponent } // Wildcard route for 404
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+//kamel el footer zidou w tfakedli el app html
