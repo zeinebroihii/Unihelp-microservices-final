962ccc5229cee86050b6333a94d034914882c866
zeinebroihii <100594821+zeinebroihii@users.noreply.github.com>
2025-04-09 13:04:29 +0100

    config Microservices final+backend+frontend

---
+++ BackOffice-UniHelp/src/app/views/base/spinners/spinners.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { DocsExampleComponent } from '@docs-components/public-api';
+import { RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, SpinnerComponent, ButtonDirective } from '@coreui/angular';
+
+@Component({
+    selector: 'app-spinners',
+    templateUrl: './spinners.component.html',
+    styleUrls: ['./spinners.component.scss'],
+    imports: [RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, DocsExampleComponent, SpinnerComponent, ButtonDirective]
+})
+export class SpinnersComponent {
+
+  constructor() { }
+
+}
