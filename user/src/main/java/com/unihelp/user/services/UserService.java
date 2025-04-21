9d7acfae14ce4c32f0eb7f22b70f04593825d6eb
Raouf2022 <abderraouf.mahmoudi@esprit.tn>
2025-04-21 22:23:13 +0100

    fixed email template

--- user/src/main/java/com/unihelp/user/services/UserService.java
+++ user/src/main/java/com/unihelp/user/services/UserService.java
@@ -4,7 +4,10 @@ import com.unihelp.user.entities.Token;
 import com.unihelp.user.repositories.TokenRepository;
 import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -83,14 +86,24 @@ public class UserService {
             String resetLink = "http://localhost:4200/login?token=" + token.getToken();
             System.out.println(resetLink);
                         try {
-                System.out.println("[DEBUG] About to send reset email to: " + email);
-                SimpleMailMessage message = new SimpleMailMessage();
-                message.setTo(email);
-                message.setSubject("Password Reset Link");
-                message.setText("Click the following link to reset your password: " + resetLink);
-                javaMailSender.send(message);
-                System.out.println("[DEBUG] Reset email sent successfully to: " + email);
-            } catch (Exception e) {
+    System.out.println("[DEBUG] About to send reset email to: " + email);
+    jakarta.mail.internet.MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+    org.springframework.mail.javamail.MimeMessageHelper helper = new org.springframework.mail.javamail.MimeMessageHelper(mimeMessage, false, "utf-8");
+    helper.setTo(email);
+    helper.setSubject("Password Change Request");
+    String htmlMsg = "<div style=\"font-family:sans-serif;background:#fff;padding:40px 0;text-align:center;\">" +
+        "  <img src=\"C:/Users/RAOUF/Desktop/PI_Cloud/Unihelp-microservices-final/frontend/src/assets/img/Unihelp-Icon.png\" alt=\"UniHelp Logo\" style=\"width:70px;margin-bottom:30px;\">" +
+        "  <h1 style=\"font-size:2.2em;margin-bottom:0.3em;\">Password Change Request</h1>" +
+        "  <p style=\"font-size:1.1em;margin-bottom:2em;\">You have submitted a password change request.</p>" +
+        "  <p style=\"max-width:500px;margin:0 auto 2em auto;font-size:1em;\">If it wasn't you please disregard this email and make sure you can still login to your account. If it was you, then <b>confirm the password change</b> <a href=\"" + resetLink + "\" style=\"color:#1976d2;text-decoration:underline;font-weight:bold;\">click here</a>.</p>" +
+        "  <br><p style=\"margin-top:2em;\">Thanks!<br><span style=\"display:inline-block;background:#000;color:#fff;padding:4px 16px;border-radius:4px;margin-top:10px;\">UniHelp Team</span></p>" +
+        "  <hr style=\"margin:40px 0 20px 0;border:none;border-top:1px solid #eee;\">" +
+        "  <div style=\"color:#b0b0b0;font-size:0.95em;\">If you did not make this request, please contact us by replying to this mail.</div>" +
+        "</div>";
+    helper.setText(htmlMsg, true);
+    javaMailSender.send(mimeMessage);
+    System.out.println("[DEBUG] Reset email sent successfully to: " + email);
+} catch (Exception e) {
                 System.out.println("[ERROR] Failed to send reset email to: " + email);
                 e.printStackTrace();
             }
